8da72b2649441b46e4df223e92a2ed43d22e2919
Aarayn10-sudo <lazyfox916@gmail.com>
2024-11-09 09:17:36 +0545

    Learning

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+//This is dist file where you would get javascript code for the code that you performed in TypeScript making it easier to learn
+//remember to use tsc in terminal
 function Test(params) {
   return params < 50000 ? params + 3 : params - 1;
 }
